4d39cc12542449f34d87feb087e5946f3cdc318e
Tài Ngô Bá Tấn <taingo2112003@gmail.com>
2025-01-13 11:03:54 +0700

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,15 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path"
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json({ limit: "50mb" })); // Tăng giới hạn JSON
@@ -24,6 +27,12 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoute);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+}
+app.get("*", (req, res) => {
+    res.sendFile(path.join(path.join(__dirname, "../frontend", "dist", "index.html")));
+})
 // Server khởi chạy
 server.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
